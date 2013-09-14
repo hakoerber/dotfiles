0a1c55b06e0db4d788f1e6aa4c6ce289985b475d
Hannes Körber <hannes.koerber@haktec.de>
2013-09-14 16:00:46 +0200

    Changed dotfile location and made path concatenation more robust.

--- scripts/makesymlinks.bash
+++ scripts/makesymlinks.bash
@@ -1,23 +1,28 @@
 #!/usr/bin/env bash
 
 # config directory
-config_dir="$HOME/config/"
+config_dir="$HOME/dotfiles/"
 
 # backup directory, files that would otherwise be overwritten go there
-backup_dir="$HOME/oldconfig/"
+backup_dir="$HOME/dotfiles.bak/"
 
 # the following folders inside $config_dir will be inspected and symlinked:
 symlink_folders="bash git i3 vim zsh"
+
+path_combine()
+{
+    echo "$(dirname "$1")/$(basename "$1")/$(basename "$2")"
+}
 
 # backup the old config files
 backup_dir="$backup_dir/$(date +%Y-%m-%dT%H:%M:%S)"
 echo "Backing up old configuration files into \"$backup_dir\"."
 mkdir -p "$backup_dir"
 for folder in $symlink_folders ; do
-    for file in "$config_dir/$folder"/* ; do
-        oldfile="$HOME/.$(basename "$file")"
+    for file in "$(path_combine "$config_dir" "$folder")"/* ; do
+        oldfile="$(path_combine "$HOME" ".$(basename "$file")")"
         if [[ -e "$oldfile" ]] ; then
-            destination="$backup_dir/$(basename "$oldfile")"
+            destination="$(path_combine "$backup_dir" "$(basename "$oldfile")")"
             echo "mv: \"$oldfile\" -> \"$destination\""
             mv "$oldfile" "$destination"
         else
@@ -29,10 +34,10 @@ done
 # now symlink the files in $config_dir into the home directory
 echo "Creating symlinks for configuration files in \"$config_dir\"."
 for folder in $symlink_folders ; do
-    for file in "$config_dir/$folder"/* ; do
-        destination="$HOME/.$(basename "$file")"
-        echo "ln -s \"$file\" -> \"$destination\""
-        ln -s "$file" "$destination"
+    for file in "$(path_combine "$config_dir" "$folder")"/* ; do
+        destination="$(path_combine "$HOME" ".$(basename "$file")")"
+        echo "ln -sf \"$file\" -> \"$destination\""
+        ln -sf "$file" "$destination"
     done
 done
 
