8d805aa69884bf24e796e6e0b9b034a0957ccf35
Hannes Körber <hannes@hkoerber.de>
2013-10-01 02:49:55 +0200

    Added configuration for mpd and ncmpcpp.

--- scripts/setup.bash
+++ scripts/setup.bash
@@ -10,7 +10,7 @@ backup_dir="$HOME/.dotfiles.bak/"
 
 # the following folders inside $config_dir will be inspected and the
 # contents symlinked:
-symlink_folders='bash git i3 vim zsh conky terminator x'
+symlink_folders='bash git i3 vim zsh conky terminator x mpd ncmpcpp'
 
 MAPPING_SEPARATOR='::'
 
