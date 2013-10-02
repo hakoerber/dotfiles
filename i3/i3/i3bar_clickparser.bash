8af7ac0d29a3aec50884744a34ab826e17fa8a9d
Hannes Körber <hannes@hkoerber.de>
2013-10-02 18:05:24 +0200

    Fixed quoting error in i3bar_clickparser.bash.

--- i3/i3/i3bar_clickparser.bash
+++ i3/i3/i3bar_clickparser.bash
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-line="$1
-
-"
+line="$1"
 
 log(){
     echo $* >> ~/.i3/output_i3bar.log.2
@@ -13,15 +11,18 @@ getval(){
     echo $(echo "$1" | grep -Po "\"$2\":.*?," | awk -F ':' '{print $2}' | tr -d '",')
 }
 
-log $line
+log "line: $line"
 
 name="$(getval "$line" "name")"
-log $name
+log "name: $name"
 
 case "$name" in
 "time")
     log "starting dzen with cal"
-    (echo "$(cal -1m)" ; sleep 15s) | dzen2 -w 100 -l 10 -x 1700 -y 20
+    (echo "$(cal -1m)" ; sleep 15s) | dzen2 -w 100 -l 10 -x 1700 -y 20 &
+    ;;
+"mpd_status")
+    mpc toggle
     ;;
 esac
 
