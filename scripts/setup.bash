870cd925c9e2030446a075c005f101dfd8263cc1
Hannes Körber <hannes.koerber@haktec.de>
2014-04-26 16:45:54 +0200

    Merge bash with zsh config and remove bash config.

--- scripts/setup.bash
+++ scripts/setup.bash
@@ -10,7 +10,7 @@ backup_dir="$HOME/.dotfiles.bak/"
 
 # the following folders inside $config_dir will be inspected and the
 # contents symlinked:
-symlink_folders='bash git i3 vim zsh conky x mpd ncmpcpp'
+symlink_folders='git i3 vim zsh conky x mpd ncmpcpp'
 
 MAPPING_SEPARATOR='::'
 
