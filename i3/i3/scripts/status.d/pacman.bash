bf90d870b5d99ede8238c36287687e9907b0a912
Hannes Körber <hannes.koerber@haktec.de>
2014-07-26 20:37:59 +0200

    Changes. And more changes.

--- i3/i3/scripts/status.d/pacman.bash
+++ i3/i3/scripts/status.d/pacman.bash
@@ -1,6 +1,9 @@
 #!/bin/bash
-
 echo "Total packages: $(pacman -Qq | wc -l)"
-echo ""
-echo "Updates available:"
-echo "$(pacman -Qu | column -t)"
+echo
+if ! pacman -Qu >/dev/null 2>&1; then
+    echo "Up to date."
+else
+    echo "Available updates:"
+    echo "$(pacman -Qu | column -t)"
+fi
