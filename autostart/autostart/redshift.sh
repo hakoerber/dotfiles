d861f33b5de1e39d64c93fb2b863fa4b120dc682
Hannes Körber <hannes.koerber@haktec.de>
2016-02-15 18:48:26 +0100

    Update redshift.

--- autostart/autostart/redshift.sh
+++ autostart/autostart/redshift.sh
@@ -1,6 +1,6 @@
 # redshift settings
 redshift_lat_long="49.5:11"
-redshift_colortemp="5500:3700"
+redshift_colortemp="6000:3300"
 
 log "starting redshift-gtk"
-redshift-gtk -l "$redshift_lat_long" -t "$redshift_colortemp" & &>> "$LOGFILE"
+redshift-gtk -b 1 -l "$redshift_lat_long" -t "$redshift_colortemp" & &>> "$LOGFILE"
