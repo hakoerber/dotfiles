f5aec331fd3124aedfc45c18e4b767f27dd9e9fb
Hannes Körber <hannes@hkoerber.de>
2016-04-02 16:00:37 +0200

    Remove wallchanger fallback, search PATH instead of hardcoded binary.

--- autostart/autostart/wallchanger.sh
+++ autostart/autostart/wallchanger.sh
@@ -1,13 +1,10 @@
-path_wallchanger="$HOME/projects/wallchanger/wallchanger"
 wallpaper_directory="$LIBDIR/wallpaper/current"
 wallchanger_pidfile="$RUNDIR/wallchanger.${XDG_SESSION_ID}.pid"
 wallpaper_logfile="$LOGDIR/wallpaper.log"
-wallpaper_fallback="$HOME/.i3/data/wallpaper/"
 wallpaper_interval="10800"
 
 log "starting $path_wallchanger"
 {
-    sleep 0
-    $path_wallchanger "$wallpaper_directory" "$wallpaper_interval" "$wallpaper_fallback" &
+    wallchanger "$wallpaper_directory" "$wallpaper_interval" &
     echo $! > "$wallchanger_pidfile"
 } & &>> $LOGFILE
