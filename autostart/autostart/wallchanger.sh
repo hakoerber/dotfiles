798a9b6ad5165f55536bc7f3d3f0991fd5742a77
Hannes Körber <hannes.koerber@haktec.de>
2016-04-02 15:52:20 +0200

    Make PID files unique for X session.

--- autostart/autostart/wallchanger.sh
+++ autostart/autostart/wallchanger.sh
@@ -1,6 +1,6 @@
 path_wallchanger="$HOME/projects/wallchanger/wallchanger"
-wallchanger_pidfile="$RUNDIR/wallchanger.pid"
 wallpaper_directory="$HOME/pictures/wallpaper/misc"
+wallchanger_pidfile="$RUNDIR/wallchanger.${XDG_SESSION_ID}.pid"
 wallpaper_logfile="$LOGDIR/wallpaper.log"
 wallpaper_fallback="$HOME/.i3/data/wallpaper/"
 wallpaper_interval="10800"
