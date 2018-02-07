7a2024f1aede800b4174c4efc0b4fa9ac323dfec
Hannes Körber <hannes@hkoerber.de>
2018-02-07 14:41:49 +0100

    Fix wallpaper

--- autostart/40_wallpaper.sh
+++ autostart/40_wallpaper.sh
@@ -5,4 +5,6 @@ wallpaper="$LIBDIR/wallpaper/current"
 printf '%s' "setting wallpaper" >>"$LOGFILE"
 {
     feh --bg-scale "${wallpaper}"
+    sleep 10
+    feh --bg-scale "${wallpaper}"
 } & &>> $LOGFILE
