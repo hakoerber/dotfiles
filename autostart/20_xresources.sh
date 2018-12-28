d8b7d9aeb8dd8f59a1815e255c24669c1f2139ee
Hannes Körber <hannes.koerber@tradebyte.com>
2018-12-28 18:19:00 +0100

    Update

--- autostart/20_xresources.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-printf '%s' "parsing .Xresources"
-xrdb -merge -I${HOME} ~/.Xresources
--- autostart/50_network.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-printf '%s' "starting network tray application"
-systemd-run --property=Restart=always --user --setenv=DISPLAY=${DISPLAY} nm-applet
