d15b2b08da923cc9a44ac249336235da9152a7b2
Hannes Körber <hannes@hkoerber.de>
2024-04-22 14:17:15 +0200

    Add generic boostrap script

---
+++ install_scripts/bootstrap.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -o nounset
+set -o errexit
+
+host="${1}"   ; shift
+
+pacman -Sy --noconfirm git # yes its a partial upgrade, but thats just the live cd
+
+cd /root
+git clone --recursive https://code.hkoerber.de/hannes/dotfiles.git
+
+./dotfiles/install_scripts/${host}.sh
+
+mv /root/dotfiles /mnt/var/lib/dotfiles
+
+read -p "> Ready for reboot. Press enter for shutdown, then remove the installation media and boot again "
+
+poweroff
