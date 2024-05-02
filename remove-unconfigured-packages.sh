d98529f30b8ca3b42564fbe63273c6358f154de7
Hannes Körber <hannes@hkoerber.de>
2024-05-02 17:10:04 +0200

    Add script to remove unconfigured packages

---
+++ remove-unconfigured-packages.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+declare -a aurdeps=()
+
+proctected=(
+  nvidia-dkms
+  intel-ucode
+  amd-ucode
+  base
+  java-runtime-common
+  jdk17-openjdk
+)
+
+for pkgbuild in pkgbuilds/*/PKGBUILD ; do
+  source "${pkgbuild}"
+  aurdeps+=("${depends[@]%%[<=>]*}" "${makedepends[@]%%[<=>]*}" "${pkgname}")
+done
+
+packages_to_remove=()
+
+readarray -d $'\0' -t packages_to_remove < <(comm --zero-terminated -13 \
+  <(cat \
+    <(<packages.yml yaml2json | jq --raw-output0 '.packages.list | map(.archlinux) | flatten[]') \
+    <(for dep in "${aurdeps[@]}" ; do printf '%s\0' "${dep}" ; done) \
+  | sort -zu) \
+  <(pacman -Qq --explicit | xargs -I "{}" printf '%s\0' "{}" | sort -zu) \
+| while IFS= read -r -d $'\0' package; do
+    skip=0
+    for protected in "${proctected[@]}" ; do
+      if [[ "${package}" == "${protected}" ]] ; then 
+        skip=1
+      fi
+    done
+    if (( skip )) ; then
+      continue
+    fi
+    printf '%s\0' "${package}"
+  done) 
+
+sudo pacman -Rcns "${packages_to_remove[@]}"
+
