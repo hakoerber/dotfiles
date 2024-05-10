fb781a5b5d3734d0115ba962502c2a3b6d6f33c4
Hannes Körber <hannes@hkoerber.de>
2024-05-10 22:00:38 +0200

    Fix script to detect missing packages

--- detect-missing-packages.py
+++ detect-missing-packages.py
@@ -9,11 +9,12 @@ apps = yaml.safe_load(open(package_file, 'r'))
 
 missing_config = {}
 
-for appname, appconfig in apps['packages']['list'].items():
+for appname, appconfig in apps.items():
     for distro, packagelist in appconfig.items():
         if len(packagelist) == 0:
             if distro not in missing_config.keys():
                 missing_config[distro] = []
             missing_config[distro].append(appname)
 
-print(yaml.dump(missing_config), end="")
+if missing_config:
+    print(yaml.dump(missing_config), end="")
