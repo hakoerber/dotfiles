09ba133747783571a5355efb2dde09e02e12d801
Hannes Körber <hannes@hkoerber.de>
2026-08-21 15:01:05 +0200

    Stop updating local packges

--- update-aur-pkgs.sh
+++ update-aur-pkgs.sh
@@ -4,16 +4,8 @@ set -o nounset
 set -o errexit
 
 for pkg in pkgbuilds/* ; do
-    if [[ -n "$(builtin cd "${pkg}" && git rev-parse --show-superproject-working-tree)" ]] ; then
-        printf "checking git submodule %s\n" "${pkg}"
-        git submodule update --remote "${pkg}"
-    else
-        printf "checking local package %s\n" "${pkg}"
-        (
-            builtin cd "${pkg}" || exit 1
-            makepkg --nodeps --nobuild --noextract --cleanbuild
-        )
-    fi
+    printf "checking git submodule %s\n" "${pkg}"
+    git submodule update --remote "${pkg}"
     if git status --porcelain "${pkg}" | grep -q . ; then
         git add "${pkg}"
         git commit -m "aur: Update $(basename "${pkg}")"
